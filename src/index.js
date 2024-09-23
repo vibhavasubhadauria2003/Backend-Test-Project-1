4c47a2daa795263095c98b57025527fee431a5f7
Vibahvasu Bhadauria <vibhavasudsvv2003@gmail.com>
2024-09-24 00:03:29 +0530

    version 7 [(12:02 AM)(24/09/2024)]

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 // require('dotenv').config({path:'./env'});
-import dotenv from "dotenv"
-import connectDB from "./db/index.js"
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-
-dotenv.config({ 
-    path:'./env' 
+dotenv.config({
+  path: "./env",
 })
 
 connectDB()
+.then(()=>{
+  app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
+
+  app.listen(process.env.PORT|| 8000,()=>{
+    console.log(`Server is runing at port : ${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log("DB connection failed",err)
+})
