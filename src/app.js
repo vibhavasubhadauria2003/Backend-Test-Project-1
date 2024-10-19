ec982018ad16ff996ddf63a2957edcd85a8aabd4
Vibahvasu Bhadauria <vibhavasudsvv2003@gmail.com>
2024-10-20 00:10:45 +0530

    version 8 [(12:10 PM)(12/10/2024)]

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
 
 const app=express()
 
+use(cors({
+    origin:process.env.CORS_LINK
+}))
 
 
 export { app }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,32 @@
-import mongoose from "mongoose";
+// import { type } from "express/lib/response";
+import mongoose, {Schema} from "mongoose";
 
-const userSchema=new mongoose.Schema({
-    
+const userSchema=new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+    avtar:{
+        type:String, //cloudinary url
+        required:true,
+    },
 })
 
 export const User = mongoose.model("User",userSchema);
