5dc0b71b5e47fb17aecfffd3de86cca206a24ae5
Vibahvasu Bhadauria <vibhavasudsvv2003@gmail.com>
2024-10-21 08:57:48 +0530

    version 8 [(08:57 AM)(21/10/2024)]

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,32 +1,50 @@
 // import { type } from "express/lib/response";
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const userSchema=new Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true,
-        index:true
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        lowercase:true,
-        trim:true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    fullname:{
-        type:String,
-        required:true,
-        trim:true,
-        index:true
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
-    avtar:{
-        type:String, //cloudinary url
-        required:true,
+    avtar: {
+      type: String, //cloudinary url
+      required: true,
     },
-})
+    coverimage: {
+      type: String, //cloudinary url
+    },
+    watchHistory: [
+      {
+        type: Schema.Type.ObjectId,
+        ref: "vedio",
+      },
+    ],
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const User = mongoose.model("User",userSchema);
+export const User = mongoose.model("User", userSchema);
