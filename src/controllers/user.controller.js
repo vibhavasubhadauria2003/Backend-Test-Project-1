c95ba19dfe182a672f2720ae5670145e6ba4721c
Vibahvasu Bhadauria <vibhavasudsvv2003@gmail.com>
2025-01-08 18:27:59 +0530

    version 15 [(06:27 PM)(08/01/2025)]

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser=asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
+    const {username,fullName,email,password} = req.body
 })
 
 export {registerUser};
