f23b9b7f8f6bea776e689bbe03bb3959483b7ce7
Vibahvasu Bhadauria <vibhavasudsvv2003@gmail.com>
2025-02-20 17:17:08 +0530

    Add routes for updating avatar, cover image, and fetching user channel profile and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,6 +7,10 @@ import {
   changePassword,
   getCurrentUser,
   updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/authentication.middleware.js";
 const router = Router();
@@ -31,5 +35,10 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changePassword);
-
-export default userRouter;
+router.route("/me").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router.route("/update-avtar").patch(verifyJWT,upload.single("avtar"), updateAvatar);
+router.route("/update-cover-image").patch(verifyJWT,upload.single("coverImage"), updateCoverImage);
+router.route("/channel/:username").get(getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
+export default router;
